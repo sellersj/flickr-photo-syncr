ef942494d64962c04a808230f83dd568e84b3fdc
Jim Sellers <jim.sellers@gmail.com>
2021-10-02 10:26:44 -0400

    adding test to capture getting file date time

--- src/test/java/com/github/sellersj/flickrps/ImageCrawlerTest.java
+++ src/test/java/com/github/sellersj/flickrps/ImageCrawlerTest.java
@@ -18,13 +18,19 @@ public class ImageCrawlerTest {
     @Test
     public void processFileGoodFile() {
         String userhome = System.getProperty("user.home");
-        // ? File f = new File(System.getProperty("user.home") + "/Downloads/16411933417_a711d12d77_o.jpg");
+        // ? File f = new File(System.getProperty("user.home") +
+        // "/Downloads/16411933417_a711d12d77_o.jpg");
         File f = new File(userhome + "/Documents/pictures/2015-01-31_dads_birthday/DSC_0916.JPG");
         ImageCrawler c = new ImageCrawler();
         Collection<MetadataTag> processFile = c.processFile(f);
 
         for (MetadataTag metadataTag : processFile) {
             System.out.println(metadataTag);
+
+            if ("File Modified Date".equals(metadataTag.getName())) {
+                String time = metadataTag.getDescription();
+
+            }
         }
     }
 
